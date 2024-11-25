660ef668a10f4a899d94445c15d63c2bdf450ccd
JohnWilson <jgabriel@abartyshealth.com>
2024-11-24 20:10:12 -0400

    Finalized merging of Existing HTML and CSS with Flask, all redirects now work.

--- Asiste_Tareas/index.py
+++ Asiste_Tareas/index.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    return render_template('Login.html')
+
+@app.route("/buscar")
+def buscar():
+    return render_template('Buscador.html')
+
+@app.route("/agregar")
+def agregar():
+    return render_template('Agregar.html')
+
+@app.route("/principal")
+def principal():
     return render_template('Principal.html')
 
 if __name__ == '__main__':
